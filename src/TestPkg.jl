4c3f0f73a67fc261251dc0b36374dd25b8693d09
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-02-02 07:35:41 +0900

    new func test

--- src/TestPkg.jl
+++ src/TestPkg.jl
@@ -1,5 +1,10 @@
 module TestPkg
-
-greet() = print("Hello World!")
-
-end # module
+    using SparseArrays
+    export test
+    function test()
+        A = spzeros(Float64, 5, 5)
+        A[1,3] = 4
+        A[2,5] = 9
+        return A
+    end
+end # end of module
